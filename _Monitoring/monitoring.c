114750e103a86d75a92098dfafd3e52db8c3b0ee
Wesley Cauan <wesleycauan01@gmail.com>
2024-10-14 16:19:04 -0300

    Ajustes no monitoring.c
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- _Monitoring/monitoring.c
+++ _Monitoring/monitoring.c
@@ -25,27 +25,15 @@ bool accountCreated = false;
 void registerForMonitoring()
 {
     cleanConsole();
-    OPERATIONALSYSTEM = verifyOperationalSystem();
-    FILE *file;
-    char pathDirectory[30];
-    if(strcmp(OPERATIONALSYSTEM, "Windows") == 0)
-    {
-      strcpy(pathDirectory, "..\\monitoring.txt");
-      file = fopen(pathDirectory, "w");
-    }
-    else
-    {
-      file = fopen("./monitoring.txt", "w");
-    }
     
     char materia[150];
     long contato;
-    int hora, minuto = 0, opcao;
+    int hora, minuto = 0, opcao, confirmarDados;
     bool repetirAcao = true;
 
     do
     {
-        printf("Digite o nome da matéria:  \n");
+        printf("Digite o nome da matéria:  \n1 - Matemática\n2 - Português\n3 - Inglês\n4 - Geografia\nResposta: ");
         scanf("%d", &opcao);
 
         switch(opcao)
@@ -75,22 +63,25 @@ void registerForMonitoring()
     do
     {
         cleanConsole();
+
+        // blocking_mode();
         do
         {
-            printf("Digite um horário disponível para você:(HH)\n");
+            printf("Digite um horário disponível para você:HH:__\n");
             scanf("%d", &hora);
+            printf(":");
             if(hora > 23 || hora < 0)
             {
                 cleanConsole();
                 printf("Por favor Digite um horário válido!!!\n");
             }
         }while(hora < 0 || hora > 23);
-        
+        // blocking_mode();
         cleanConsole();
         
         do
         {
-          printf("Digite um horário disponível para você:(MM)\n%d:", hora);
+          printf("Digite um horário disponível para você%d:MM:\n%d:", hora, hora);
           scanf("%d", &minuto);
           if(minuto < 0 || minuto > 59)
           {
@@ -102,9 +93,25 @@ void registerForMonitoring()
         
         cleanConsole();
         printf("Horário disponível %.2d:%.2d está certo? \n1 - Sim\n2 - Não\n", hora, minuto);
-        scanf("%d", &opcao);
-        if(opcao == 1)
+        scanf("%d", &confirmarDados);
+        cleanConsole();
+        if(confirmarDados == 1)
         {
+          OPERATIONALSYSTEM = verifyOperationalSystem();
+          FILE *file;
+          char pathDirectory[30];
+          if(strcmp(OPERATIONALSYSTEM, "Windows") == 0)
+          {
+            strcpy(pathDirectory, "..\\monitoring.txt");
+            file = fopen(pathDirectory, "a+");
+          }
+          else
+          {
+            file = fopen("../monitoring.txt", "a+");
+          }
+
+          rewind(file);
+
           fprintf(file, "CONTATO: %ld,", contato);
           fprintf(file, "HORA DISPONÍVEL: %d:%d", hora, minuto);
           fprintf(file, "\n");
@@ -122,16 +129,18 @@ void verifyAccountCreated()
   int newRegister;
   if(accountCreated == true)
   {
+    cleanConsole();
     printf("Você já está cadastrado na monitoria.\nSe continuar essa ação seus antigos registros serão perdidos\nDeseja continuar?\n1 - Sim\n2 - Não\nResposta: ");
     scanf("%d", &newRegister);
     if(newRegister == 1)
     {
+      accountCreated = !accountCreated;
       registerForMonitoring();
     }
     else if(newRegister == 2)
     {
-      accountCreated = !accountCreated;
-      menuLogado();
+      cleanConsole();
+      printf("Ok!!!\n");
     }
   }
   else
