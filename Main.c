424b53f36185a509f84a9bf568cfcf27313ac671
Wesley Cauan <wesleycauan01@gmail.com>
2024-10-16 17:08:58 -0300

    Alterado os parâmetros para verificar se o usuário tem conta ou não. e passando o usuário logado
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- Main.c
+++ Main.c
@@ -85,7 +85,7 @@ int main()
         }
         else if(opcao == 5)
         {
-            verifyAccountCreated();
+            verifyAccountCreated(&loggedUser);
         }
         else
         {
